7a6a159c89ab59e123d637273b63aa7b55408f7b
Gözde <altuntasgozde@gmail.com>
2022-11-11 22:52:52 +0300

    main structure

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [mainArea, setMainArea] = useState();
+
+  const GetColor = (event) => {
+    if (event == 1) {
+      setMainArea(styles.red);
+    } else if (event == 2) {
+      setMainArea(styles.blue);
+    } else if (event == 3) {
+      setMainArea(styles.green);
+    } else {
+      setMainArea(styles.pink);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container,mainArea]}>
+      <View>
+        <StatusBar style="auto" />
+        <View>
+          <Text>Main Area</Text>
+        </View>
+        <View style={styles.wrap}>
+          <Pressable value="red" onPress={() => GetColor(1)}>
+            <Text style={styles.red}>Kırmızı</Text>
+          </Pressable>
+          <Pressable value="blue" onPress={() => GetColor(2)}>
+            <Text style={styles.blue}>Mavi</Text>
+          </Pressable>
+          <Pressable value="green" onPress={() => GetColor(3)}>
+            <Text style={styles.green}>Yeşil</Text>
+          </Pressable>
+          <Pressable value="pink" onPress={() => GetColor(4)}>
+            <Text style={styles.pink}>Pembe</Text>
+          </Pressable>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +46,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  wrap: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 50,
+  },
+  red: {
+    backgroundColor: "red",
+    padding: 20,
+  },
+  blue: {
+    backgroundColor: "blue",
+    padding: 20,
+  },
+  green: {
+    backgroundColor: "green",
+    padding: 20,
+  },
+  pink: {
+    backgroundColor: "pink",
+    padding: 20,
   },
 });
